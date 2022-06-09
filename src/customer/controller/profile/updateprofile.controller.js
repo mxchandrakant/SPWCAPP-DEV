bed8ad213acdc776896140561d795dca73c3ce74
SOtari <swapnil@maxdigi.com>
2022-06-09 18:56:38 +0530

    jhjj

--- src/customer/controller/profile/updateprofile.controller.js
+++ src/customer/controller/profile/updateprofile.controller.js
@@ -6,8 +6,6 @@ const updateProfile = async(req,res) => {
     }catch(error){
         return res.status(400).send({ success:false,message:ecode.profile.SYSC0201.desc, error_code:ecode.profile.SYSC0201, data:{ error:error } })
     }
-
-    
 }
 
 module.exports = {
